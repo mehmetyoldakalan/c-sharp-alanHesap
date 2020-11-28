481a42c7dd05cb69ad84cb0c44e0cd0813579efa
mehmet yoldakalan <52545184+mehmetyoldakalan@users.noreply.github.com>
2020-11-28 23:22:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,34 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NesneTabanlıProgramlamaOdev2
+namespace NesneTabanlıProgramlamaOdev
 {
-    class Dikdortgen
-    {
-        public int b;
-        public int h;
-
-        public int alanhesapla()
-        {
-            int sonuc = b * h;
-            return sonuc;
-        }
-
-        public int cevrehesapla()
-        {
-            int sonuc = (2 * b) + (2 * h);
-            return sonuc;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Dikdortgen dikdortgen = new Dikdortgen();
-            Console.WriteLine("b ve h için değer giriniz");
-            dikdortgen.b = Convert.ToInt32(Console.ReadLine());
-            dikdortgen.h = Convert.ToInt32(Console.ReadLine());
-            
+            Musteri musteri = new Musteri();
+            Console.WriteLine("isim giriniz");
+            musteri.Ad = (Console.ReadLine());
+            Console.WriteLine("soyisim giriniz");
+            musteri.Soyad = Console.ReadLine();
+            Console.WriteLine("varsa öğrenci no giriniz ");
+            musteri.OgrNo =Console.ReadLine();
+            Console.WriteLine("yaşınızı giriniz");
+            musteri.Yas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("varsa meslek giriniz");
+            musteri.Meslek = Console.ReadLine();
+            Console.WriteLine("varsa maaş girininz");
+            musteri.Maas = (Console.ReadLine());
+
+            if (musteri.Meslek == string.Empty)
+            {
+                Console.WriteLine(musteri.Ad +" "+ musteri.Soyad +" "+ musteri.Yas +" "+ musteri.OgrNo);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.Yas + " " + musteri.Meslek+" "+musteri.Maas);
+                Console.ReadLine();
+            }
         }
     }
 }
