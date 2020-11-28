8d206bacdd27c62c250001477ed725bfe4573104
mehmet yoldakalan <52545184+mehmetyoldakalan@users.noreply.github.com>
2020-11-28 23:31:28 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NesneTabanlıProgramlamaOdev
+namespace NesneTabanlıProgramlamaOdev2
 {
+    class Dikdortgen
+    {
+        public int b;
+        public int h;
+
+        public int alanhesapla()
+        {
+            int sonuc = b * h;
+            return sonuc;
+        }
+
+        public int cevrehesapla()
+        {
+            int sonuc = (2 * b) + (2 * h);
+            return sonuc;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Musteri musteri = new Musteri();
-            Console.WriteLine("isim giriniz");
-            musteri.Ad = (Console.ReadLine());
-            Console.WriteLine("soyisim giriniz");
-            musteri.Soyad = Console.ReadLine();
-            Console.WriteLine("varsa öğrenci no giriniz ");
-            musteri.OgrNo =Console.ReadLine();
-            Console.WriteLine("yaşınızı giriniz");
-            musteri.Yas = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("varsa meslek giriniz");
-            musteri.Meslek = Console.ReadLine();
-            Console.WriteLine("varsa maaş girininz");
-            musteri.Maas = (Console.ReadLine());
-
-            if (musteri.Meslek == string.Empty)
-            {
-                Console.WriteLine(musteri.Ad +" "+ musteri.Soyad +" "+ musteri.Yas +" "+ musteri.OgrNo);
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " " + musteri.Yas + " " + musteri.Meslek+" "+musteri.Maas);
-                Console.ReadLine();
-            }
+            Dikdortgen dikdortgen = new Dikdortgen();
+            Console.WriteLine("b ve h için değer giriniz");
+            dikdortgen.b = Convert.ToInt32(Console.ReadLine());
+            dikdortgen.h = Convert.ToInt32(Console.ReadLine());
+            
         }
     }
 }
